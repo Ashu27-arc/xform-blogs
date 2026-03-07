0bd47956b4f5c8d4eaf19e7f48b2369b9bb163a6
Ashu27-arc <ashrathor0290@gmail.com>
2026-03-07 12:47:04 +0530

    ok

--- src/components/embededBanner-5.tsx
+++ src/components/embededBanner-5.tsx
@@ -3,7 +3,7 @@ import EmbededBanner from './embededBanner'
 export default function EmbededBanner5() {
   return (
     <EmbededBanner
-      src="/neet-bhaiya.webp"
+      src="/pg-counselling.webp"
       alt="Radical banner 5"
       className="w-full max-w-[420px]"
     />
