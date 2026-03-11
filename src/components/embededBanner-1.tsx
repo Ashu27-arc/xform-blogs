d588d42757f54df8e6af9fe99ae4cdb581d790e9
Ashu27-arc <ashrathor0290@gmail.com>
2026-03-11 11:50:36 +0530

    ok

--- src/components/embededBanner-1.tsx
+++ src/components/embededBanner-1.tsx
@@ -3,7 +3,7 @@ import EmbededBanner from './embededBanner'
 export default function EmbededBanner1() {
   return (
     <EmbededBanner
-      src="/phone-in-hand.webp"
+      src="/study-abroad.webp"
       alt="Radical banner 1"
       className="w-full max-w-[800px]"
     />
