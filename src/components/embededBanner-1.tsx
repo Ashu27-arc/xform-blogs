ba4068d9e423495d38dd3d06a6c11a5281351cb0
Ashu27-arc <ashrathor0290@gmail.com>
2026-03-11 10:52:13 +0530

    ok

--- src/components/embededBanner-1.tsx
+++ src/components/embededBanner-1.tsx
@@ -3,7 +3,7 @@ import EmbededBanner from './embededBanner'
 export default function EmbededBanner1() {
   return (
     <EmbededBanner
-      src="/Facebook-cover.png"
+      src="/facebook-cover.webp"
       alt="Radical banner 1"
       className="w-full max-w-[600px]"
     />
